3929befda59170685f0dc3998afcd4bc2ff5e07d
mitibhat <miti.bhat@gmail.com>
2025-08-14 08:43:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http'); // Import the http module
+
+// Create a server object
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send a response
+  res.end('Hello, this is a simple Node.js web server!');
+});
+
+// Specify the port on which the server will listen
+const port = 3000;
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
